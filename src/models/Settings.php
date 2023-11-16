dea753adec2fcc9023e5e1b5a27d816e3f0842c8
Alyxia Sother <keanu@brik.digital>
2023-11-16 16:45:58 +0100

    [models/Settings] Only allow environment variables

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -2,7 +2,6 @@
 
 namespace brikdigital\statuspaginator\models;
 
-use Craft;
 use craft\base\Model;
 
 /**
@@ -23,8 +22,8 @@ class Settings extends Model
 
     public function validateToken($attribute, $params, $validator)
     {
-        if (!preg_match(self::ENV_REGEX, $this->$attribute) || strlen($this->$attribute) === 180) {
-           $this->addError($attribute, 'The token must be an environment variable or a 180 character string.');
+        if (!preg_match(self::ENV_REGEX, $this->$attribute)) {
+            $this->addError($attribute, 'The token must be an environment variable.');
         }
     }
 }
