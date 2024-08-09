4f3d1bbfd4ecb194d2f98e2ff64d1a186ffa1e77
Alyxia Sother <alyxia@riseup.net>
2024-08-09 11:40:05 +0200

    [StatusController] Disable CSRF validation

--- src/controllers/StatusController.php
+++ src/controllers/StatusController.php
@@ -19,31 +19,8 @@ use yii\web\Response;
  */
 class StatusController extends Controller
 {
-    /**
-     * @var string the ID of the action that is used when the action ID is not specified
-     * in the request. Defaults to 'index'.
-     */
-    public $defaultAction = 'index';
-
-    /**
-     * @var int|bool|int[]|string[] Whether this controller’s actions can be accessed anonymously.
-     *
-     * This can be set to any of the following:
-     *
-     * - `false` or `self::ALLOW_ANONYMOUS_NEVER` (default) – indicates that all controller actions should never be
-     *   accessed anonymously
-     * - `true` or `self::ALLOW_ANONYMOUS_LIVE` – indicates that all controller actions can be accessed anonymously when
-     *    the system is live
-     * - `self::ALLOW_ANONYMOUS_OFFLINE` – indicates that all controller actions can be accessed anonymously when the
-     *    system is offline
-     * - `self::ALLOW_ANONYMOUS_LIVE | self::ALLOW_ANONYMOUS_OFFLINE` – indicates that all controller actions can be
-     *    accessed anonymously when the system is live or offline
-     * - An array of action IDs (e.g. `['save-guest-entry', 'edit-guest-entry']`) – indicates that the listed action IDs
-     *   can be accessed anonymously when the system is live
-     * - An array of action ID/bitwise pairs (e.g. `['save-guest-entry' => self::ALLOW_ANONYMOUS_OFFLINE]` – indicates
-     *   that the listed action IDs can be accessed anonymously per the bitwise int assigned to it.
-     */
     protected $allowAnonymous = self::ALLOW_ANONYMOUS_LIVE;
+    public $enableCsrfValidation = false;
 
     /**
      * _statuspaginator/status action
